755ca7c0bcd9278ba8e949ef415b2602bcbb7918
rythorpe <ryan_thorpe@brown.edu>
2022-04-22 00:44:15 -0400

    plot spectral ratios

--- alpha_analysis.py
+++ alpha_analysis.py
@@ -4,7 +4,7 @@ import os.path as op
 from glob import glob
 
 import numpy as np
-from scipy.stats import zscore
+from scipy.stats import zscore, norm
 import matplotlib.pyplot as plt
 from joblib import Parallel, delayed
 
@@ -17,11 +17,20 @@ from mne.report import Report
 mne.viz.set_browser_backend('qt')
 
 
+def conf_int_mean(data, conf=0.95):
+    n = data.shape[0]
+    mean = data.mean(axis=0)
+    std_mean = data.std(axis=0, ddof=1) / n**(1/2)
+    lb = norm.ppf((1 - conf) / 2, loc=mean, scale=std_mean)
+    ub = norm.ppf((1 + conf) / 2, loc=mean, scale=std_mean)
+    return mean, lb, ub
+
+
 def ssd_alpha(raw_sessions_filtered, ssd, num_filters=5):
 
     fig_ssd, axes = plt.subplots(num_filters, 2, figsize=[10, 10],
                                  sharey='col')
-    colors = ['k', 'grey']
+    colors = ['grey', 'orange']
     n_epochs = 100
     dt = 1/raw_sessions_filtered[0].info['sfreq']
     times = raw_sessions_filtered[0].times
@@ -34,6 +43,8 @@ def ssd_alpha(raw_sessions_filtered, ssd, num_filters=5):
     for filt_idx in range(num_filters):
 
         # plot SSD filter topography
+        # XXX SSD.filters_ or SSD.patterns_???
+        # https://mne.tools/stable/generated/mne.decoding.SSD.html#mne.decoding.SSD.get_spectral_ratio  # noqa
         im, _ = plot_topomap(ssd.patterns_[filt_idx], ssd.info,
                              axes=axes[filt_idx, 0], show=False)
         plt.colorbar(im, ax=axes[filt_idx, 0])
@@ -70,14 +81,13 @@ def ssd_alpha(raw_sessions_filtered, ssd, num_filters=5):
             avg_filtered_psd = np.mean(raw_filtered_psds, axis=0)
 
             session_alpha[sess_idx] = raw_filtered_psds[:, alpha_mask]
-            label = f'session {sess_idx}'
             # label = (r"session {0}" "\n" r"alpha mean: {1:.2e}" "\n"
             #          r"alpha std: {2:.2e}").format(sess_idx + 1,
             #                                        alpha_max_mean,
             #                                        alpha_max_std)
             # plot epoch-avg PSD
             axes[filt_idx, 1].plot(freqs, avg_filtered_psd,
-                                   c=color, label=label)
+                                   c=color, label=f'session {sess_idx + 1}')
             axes[filt_idx, 1].set_ylabel('power')
             axes[filt_idx, 1].set_xlabel('freq. (Hz)')
             axes[filt_idx, 1].legend()
@@ -87,11 +97,18 @@ def ssd_alpha(raw_sessions_filtered, ssd, num_filters=5):
 
 def ssd_spec_ratios(raw_sessions_filtered, ssd):
 
-    fig_spec_ratios, axes = plt.subplots(1, 2, sharex=True, sharey=True)
+    n_ssd_dims = ssd.patterns_.shape[0]
+    fig_spec_ratios, axes = plt.subplots(1)
+    colors = ['grey', 'orange']
 
     data_agg = np.stack([raw_filtered.get_data() for raw_filtered
                          in raw_sessions_filtered], axis=0)
     spec_ratio_agg, _ = ssd.get_spectral_ratio(data_agg)
+
+    # store stats of comparison across sessions
+    spec_ratio_stats = {'mean': np.zeros([2, n_ssd_dims]),
+                        'lb': np.zeros([2, n_ssd_dims]),
+                        'ub': np.zeros([2, n_ssd_dims])}
 
     for sess_idx, raw_filtered in enumerate(raw_sessions_filtered):
         epochs = mne.make_fixed_length_epochs(raw_filtered,
@@ -99,17 +116,46 @@ def ssd_spec_ratios(raw_sessions_filtered, ssd):
                                               reject_by_annotation=False,
                                               proj=False)
 
-        for data_epoch in epochs.__iter__():
-            spec_ratio_epochs, _ = ssd.get_spectral_ratio(data_epoch)
-            axes[sess_idx].plot(spec_ratio_epochs, color='orange',
-                                alpha=0.6, label='epoch-specific eigenvalues')
-        axes[sess_idx].plot(spec_ratio_agg, color='black',
-                            label='agg. eigenvalues')
-        axes[sess_idx].set_xlabel("Eigenvalue Index")
-        axes[sess_idx].set_ylabel(r"Spectral Ratio $\frac{P_f}{P_{sf}}$")
-        # axes[sess_idx].legend()
-        axes[sess_idx].axhline(1, linestyle='--')
-    
+        n_epochs = epochs.get_data().shape[0]
+        spec_ratio_epochs = np.zeros([n_epochs, n_ssd_dims])
+        for idx, data_epoch in enumerate(epochs.__iter__()):
+            spec_ratio_epochs[idx, :], _ = ssd.get_spectral_ratio(data_epoch)
+            # axes.plot(spec_ratio_epochs, color=colors[sess_idx],
+            #           marker=',', linewidth=0., alpha=0.5)
+
+        sess_mean, lb, ub = conf_int_mean(spec_ratio_epochs, conf=0.95)
+        spec_ratio_stats['mean'][sess_idx, :] = sess_mean
+        spec_ratio_stats['lb'][sess_idx, :] = lb
+        spec_ratio_stats['ub'][sess_idx, :] = ub
+
+        axes.plot(sess_mean, color=colors[sess_idx],
+                  linewidth=0.5, label=f'session {sess_idx + 1}')
+        axes.fill_between(x=range(n_ssd_dims), y1=lb, y2=ub, linewidth=0,
+                          color=colors[sess_idx], alpha=0.5)
+
+    axes.plot(spec_ratio_agg, color='black', linewidth=0.5, linestyle='--',
+              alpha=0.7, label='aggregate')
+    axes.set_xlabel("Eigenvalue Index")
+    axes.set_ylabel(r"Spectral Ratio $\frac{P_f}{P_{sf}}$")
+
+    # find highest SSD component where the mean spectral ratio is different
+    sess_argmin = spec_ratio_stats['mean'].argmin(axis=0)
+    for filter_idx in range(n_ssd_dims):
+        argmin = sess_argmin[filter_idx]
+        min_mean = spec_ratio_stats['mean'][argmin, filter_idx]
+        min_ub = spec_ratio_stats['ub'][argmin, filter_idx]
+        max_mean = spec_ratio_stats['mean'][argmin - 1, filter_idx]
+        max_lb = spec_ratio_stats['lb'][argmin - 1, filter_idx]
+
+        # XXX very conservative
+        if (min_ub < max_lb):
+            axes.vlines(filter_idx, 0.98, 1.02,
+                        label=f'SSD component {filter_idx}')
+            break
+
+    axes.legend()
+    axes.axhline(1, linestyle='--')
+
     return fig_spec_ratios
 
 
@@ -139,6 +185,8 @@ def analysis(subj_id):
     session_fnames = sorted(glob(op.join(data_dir, subj_id + '*')))
 
     fig_alpha_topo, axes = plt.subplots(1, 3, figsize=(9, 3))
+    colors = ['grey', 'orange']
+
     for session_idx, fname in enumerate(session_fnames):
         raw = mne.io.read_raw_ctf(fname, verbose=False)
         raw.load_data()
@@ -155,13 +203,14 @@ def analysis(subj_id):
                                                fmin=1., fmax=50.)
         # plot channel-mean psd
         chan_avg_psd = raw_psds.mean(axis=0)
-        axes[0].plot(freqs, chan_avg_psd)
+        axes[0].plot(freqs, chan_avg_psd, color=colors[session_idx],
+                     alpha=0.8)
         axes[0].set_ylabel('power')
         axes[0].set_xlabel('freq. (Hz)')
         # plot alpha topography
         alpha_mask = np.logical_and(freqs >= 9, freqs <= 14)
         avg_alpha_pow = raw_psds[:, alpha_mask].mean(axis=1)
-        im, _ = plot_topomap(avg_alpha_pow, raw.info,
+        im, _ = plot_topomap(avg_alpha_pow, raw.info, vmax=70,
                              axes=axes[session_idx + 1],
                              show=False)
         plt.colorbar(im, ax=axes[session_idx + 1])
@@ -185,22 +234,22 @@ def analysis(subj_id):
 
 
 if __name__ == '__main__':
-    n_jobs = 1
+    n_jobs = 6
     data_dir = '/home/ryan/Documents/datasets/MDD_ketamine_2022'
     subj_ids = list({fname[:8] for fname in listdir(data_dir)})
     print(subj_ids)
 
-    subj_ids = subj_ids[:5]
-    subj_ids = ['RASMDGZN']
+    subj_ids = subj_ids[:20]
+    # subj_ids = ['RASMDGZN']
 
     out = Parallel(n_jobs=n_jobs)(delayed(analysis)(subj_id) for subj_id
                                   in subj_ids)
-    #fig_alpha_topo_list, fig_ssd_list, sessions_alpha_list = zip(*out)
+    # fig_alpha_topo_list, fig_ssd_list, sessions_alpha_list = zip(*out)
 
     report = Report()
     for fig_alpha_topo, fig_ssd, _, fig_spec_ratios, subj_id in out:
         report.add_figure(fig_alpha_topo, title=subj_id, tags=('topo',))
         report.add_figure(fig_ssd, title=subj_id, tags=('ssd_psd',))
-        report.add_figure(fig_spec_ratios, title=subj_id, tags=('spec_ratios',))
+        report.add_figure(fig_spec_ratios, title=subj_id,
+                          tags=('spec_ratios',))
     report.save('alpha_analysis.html', overwrite=True, open_browser=True)
-    
